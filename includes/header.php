4e4491ab94e2d83e4a04a5278150d399211f3ca0
risimoDev <bzk@risimolev.ru>
2025-04-17 17:01:04 +0500

    header

--- includes/header.php
+++ includes/header.php
@@ -12,6 +12,7 @@ unset($_SESSION['notifications']); // Очищаем уведомления по
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php echo htmlspecialchars($pageTitle ?? 'Типография'); ?></title>
   <link rel="stylesheet" href="../assets/css/style.css">
+  <link rel="stylesheet" href="../assets/css/output.css">
   <script src="https://cdn.tailwindcss.com"></script>
   <script>
     tailwind.config = {
